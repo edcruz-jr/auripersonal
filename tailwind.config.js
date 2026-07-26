5d3028a796018a3b65765f2efef6fef2134aa366
Edson Cruz Junior <edcruz_jr@outlook.com>
2026-07-25 22:49:19 -0300

    :construction: Configurando o TS e primeira seção da Landing Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,16 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                brand: {
+                    neon: '#d4ff00',
+                    darkest: '#0F1D26',  // O tom mais escuro, ótimo para rodapés ou textos principais
+                    dark: '#162C40',     // Azul marinho profundo, excelente para fundos de seções e navegação
+                    primary: '#254559',  // Cor principal, boa para botões de destaque ou headers
+                    accent: '#54778C',   // Tom mais suave, perfeito para bordas, ícones ou botões secundários
+                    light: '#F2F2F2',
+                }
+            }
         },
     },
 
