43800e2360e67da23815886acb3ea3f5572c5100
Edson Cruz Junior <edcruz_jr@outlook.com>
2026-08-05 13:33:43 -0300

    :construction: Retirando validação da tela de Cadastro
    Retirando a validação de data

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -81,10 +81,6 @@ const validateStep = () => {
         const birthDate = new Date(form.birth_date);
         const today = new Date();
         const age = today.getFullYear() - birthDate.getFullYear();
-        const monthDiff = today.getMonth() - birthDate.getMonth();
-        if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
-            age--;
-        }
         if (age < 10) {
             localErrors.value.birth_date = 'Você deve ter pelo menos 10 anos.';
             return false;
